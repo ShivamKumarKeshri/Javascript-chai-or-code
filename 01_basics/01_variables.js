b672c70e55a63a144653c1eec46e653231c09e25
Shivam kumar <79019745+Shivam-art-creator@users.noreply.github.com>
2024-09-22 17:19:35 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = '12345';
+
+let accountEmail = "shivamkeshri3580@gmail.com";
+
+var accountPassword = "23456";
+
+accountCity = "Jaipur";
+
+// accountId = 1 // Not allowed in const -  Assignment to constant variable.
+
+console.log(accountId);
+
+accountEmail = "sk@gmail.com";
+accountPassword = "23134";
+accountCity = "Bengaluru";
+
+console.table([accountEmail, accountPassword, accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello")  // we are using nodejs, not browser
+
+let name = "Shivam";
+
+let age = 24;
+
+let isLoggedIn = false 
+
+// null => standlone value
+// undefined => value is not assigned yet.
+
+console.log(typeof undefined)  // undefined
+console.log(typeof null)    // object

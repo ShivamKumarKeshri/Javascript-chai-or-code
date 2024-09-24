c134cf8ab9791d1253785bcca1e22a5e31ba511d
Shivam Kumar <79019745+ShivamKumarQ2@users.noreply.github.com>
2024-09-24 08:20:46 +0000

    Datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,10 @@
+// Docs : https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// Primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Non Primitive : Reference type Datatypes
+// Array, objects, functions
+
+// JavaScript is dynamically typed, meaning that the type of a variable is determined at runtime, based on the value assigned to it.
